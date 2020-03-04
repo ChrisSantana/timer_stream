b76d758670d22c9936552ff204102c280de9917c
Christiano Santos Ferreira de Santana <chris.santana.si@gmail.com>
2020-03-04 20:31:34 -0300

    Commit inicial do projeto

---
+++ lib/bloc/timer_bloc.dart
@@ -0,0 +1,69 @@
+import 'dart:async';
+
+const iniciar = 'Iniciar';
+const continuar = 'Continuar';
+const pause = 'Pausar';
+
+class TimerBloc {
+  int _countSeconds = 0;
+  int _countMinutes = -1;
+
+  StreamController<int> _controllerSeconds;
+  StreamController<int> _controllerMinutes;
+  StreamController<String> _controllerButton;
+
+  Timer _timer;
+
+  TimerBloc(){
+    _controllerSeconds = StreamController<int>();
+    _controllerMinutes = StreamController<int>();
+    _controllerButton = StreamController<String>();
+
+    _controllerButton.sink.add(iniciar);
+  }
+
+  Stream<int> get streamSeconds {
+    return _controllerSeconds.stream;
+  }
+
+  Stream<int> get streamMinutes {
+    return _controllerMinutes.stream;
+  }
+
+  Stream get streamButton {
+    return _controllerButton.stream;
+  }
+
+  void _onPlay(){
+    _timer = Timer.periodic(Duration(seconds: 1), (_){
+      if (!_controllerSeconds.isClosed){
+        if (_countSeconds == 0){
+          _controllerMinutes.sink.add(++_countMinutes);
+        }
+
+        _controllerSeconds.sink.add(_countSeconds++);
+
+        if (_countSeconds == 60){
+          _countSeconds = 0;
+        }
+      }
+    });
+  }
+
+  void _onPause() {
+    if (_timer != null){
+      _timer.cancel();
+      _timer = null;
+    }
+  }
+
+  void onPressed() {
+    if (_timer != null && _timer.isActive){
+      _controllerButton.sink.add(continuar);
+      _onPause();
+    } else {
+      _controllerButton.sink.add(pause);
+      _onPlay();
+    }
+  }
+}
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:timer_stream_app/bloc/timer_bloc.dart';
+
+class HomeScreen extends StatelessWidget {
+  final TimerBloc _timerBloc = TimerBloc();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Timer'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                StreamBuilder<String>(
+                  stream: _timerBloc.streamMinutes.map((value){
+                    return value.toString();
+                  }),
+                  builder: (_, snapshot){
+                    final textAux = snapshot.data ?? '';
+                    return Text(textAux, style: TextStyle(fontSize: 40),);
+                  },
+                ),
+                Text(':', style: TextStyle(fontSize: 40),),
+                StreamBuilder<String>(
+                  stream: _timerBloc.streamSeconds.map((value){
+                    return value.toString().padLeft(2, '0');
+                  }),
+                  builder: (_, snapshot){
+                    final textAux = snapshot.data ?? '';
+                    return Text(textAux, style: TextStyle(fontSize: 40),);
+                  },
+                ),
+              ],
+            ),
+            SizedBox(height: 20,),
+            StreamBuilder<String>(
+                stream: _timerBloc.streamButton,
+                initialData: 'Play',
+                builder: (context, snapshot) {
+                  return RaisedButton(
+                    child: Text(snapshot.data),
+                    onPressed: _timerBloc.onPressed,
+                  );
+                }
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
